cfc8fe136feb9e179d372b00f5a78c2fd2b23ba7
abraaoaugusto1 <160982676+abraaoaugusto1@users.noreply.github.com>
2026-07-29 13:27:24 +0000

    imagem para diversas resoluções.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ function App() {
 
   return (
     <>
-      <img src="https://picsum.photos/400/300" className="image" />
+    <picture>
+      <source media="(min-width: 1000px)" srcSet="https://picsum.photos/id/5/1200/1200"/>
+      <source media="(min-width: 750px)" srcSet="https://picsum.photos/id/5/1000/1000"/>
+      <source media="(min-width: 500px)" srcSet="https://picsum.photos/id/5/750/750"/>
+      <img src="https://picsum.photos/id/5/500/500" />
+    </picture>
     </>
   )
 }
