7854635a8d74ee33f8110847ac7eef4348a9aa6a
abraaoaugusto1 <160982676+abraaoaugusto1@users.noreply.github.com>
2026-07-29 12:14:01 +0000

    Exibe uma imagem

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import logo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <img src="https://picsum.photos/400/300" width="400px"/>
+    </>
+  )
+}
+
+export default App
